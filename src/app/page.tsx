d15a7581a793ca6ef6d20f585dfe035c7f0e1a32
Shahrear Ahamed <its.shahrear@gmail.com>
2024-12-06 00:12:23 +0600

    [FIX] change folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import ComingHome from "./(coming)/page";
+import Header from "./(coming)/_components/header";
+import Hero from "./(coming)/_components/hero";
+import Contact from "./(coming)/_components/contact";
+import Footer from "./(coming)/_components/footer";
+import Countdown from "./(coming)/_components/countdown";
 
-export default function Home() {
-  return <ComingHome />;
+export default function ComingHome() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <Countdown />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
